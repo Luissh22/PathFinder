bcbdcb1aa5ed0a82c7e68f4f8af86a6f86c7e3e9
Luissh22 <luisabraham_11@hotmail.com>
2020-12-24 12:57:06 -0500

    Initial Commit

---
+++ PathFinder/PathFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathFinderApp.swift
+//  PathFinder
+//
+//  Created by Luis Abraham on 2020-12-24.
+//
+
+import SwiftUI
+
+@main
+struct PathFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
